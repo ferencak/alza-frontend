3a7836c0a8a8bad56049a70220583e877dfee746
Nikolas Ferenčák <experimentproduction@gmail.com>
2022-10-19 16:25:16 +0200

    ErrorBoundary design improved

--- src/components/hoc/error-boundary/ErrorBoundary.tsx
+++ src/components/hoc/error-boundary/ErrorBoundary.tsx
@@ -10,9 +10,9 @@ interface IErrorBoundaryProps {
 const ErrorBoundary: FC<IErrorBoundaryProps> = ({ error: { message }, children }): JSX.Element => (
   <>
     {message && (
-      <div className="absolute right-10 top-10 w-96 h-96 bg-red-500">
-        <div className="flex flex-col items-center justify-center h-full">
-          <div className="text-2xl text-white">An error has occurred</div>
+      <div className="absolute right-10 top-10 w-96 bg-red-500 rounded-lg shadow-2xl p-4">
+        <div className="flex flex-col items-start justify-center h-full">
+          <div className="text-2xl text-white">An error has occurred!</div>
           <div className="text-white">{message}</div>
         </div>
       </div>
