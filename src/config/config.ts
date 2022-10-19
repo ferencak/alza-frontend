6e14aaff4d95163377498e48c8496d389347bc11
Nikolas Ferenčák <experimentproduction@gmail.com>
2022-10-19 16:22:47 +0200

    RestAPI endpoints

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   // Due to cors issues, we have to proxy the request to the backend
   restApi: 'https://alza-corsproxy.herokuapp.com/https://www.alza.cz/Services/RestService.svc/v2',
+  // restApi: 'http://localhost:8080/https://www.alza.cz/Services/RestService.svc/v2',
 }
