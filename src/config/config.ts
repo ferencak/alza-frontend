40b3e490e4c0b9f8d423c7e85d737837233dc25a
Nikolas Ferenčák <experimentproduction@gmail.com>
2022-10-19 14:46:17 +0200

    Changed RestAPI url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   // Due to cors issues, we have to proxy the request to the backend
-  restApi: 'http://localhost:8080/https://www.alza.cz/Services/RestService.svc/v2',
+  restApi: 'https://alza-corsproxy.herokuapp.com/https://www.alza.cz/Services/RestService.svc/v2',
 }
