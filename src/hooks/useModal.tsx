dc40735c14861ad1bcb1e0edb67786080d713e46
Nikolas Ferenčák <experimentproduction@gmail.com>
2022-10-19 14:39:36 +0200

    Modal removed

--- src/hooks/useModal.tsx
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react'
-
-import { IModalProviderProps, ModalContext } from 'contexts/modal.context'
-
-export const useModal = () => useContext<IModalProviderProps>(ModalContext)
